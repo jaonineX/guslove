e7cb07b925bc13854d12694c3c1df4e7aa3ebe53
jaonineX <neoking285@gmail.com>
2024-03-22 01:38:41 +0700

    .

---
+++ Home.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+gender_data=pd.read_csv('./data/Transformed Data Set - Sheet1.csv')
+
+
+html_0 = """
+<div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
+<center><h3>Example data table</h3></center>
+</div>
+"""
+
+
+
+st.markdown(html_0, unsafe_allow_html=True)
+st.markdown("")
+st.write(gender_data.head(10))
+
+
+
+html_1 = """
+<div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
+<center><h3>The relationship between (Favorite Color) and (Gender)</h3></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
+
+
+
+plt.figure(figsize=(15,5))
+sns.countplot(x='Gender', data=gender_data, hue='Favorite Color')
+plt.title('Count of Favorite Colors by Gender')
+plt.xlabel('Gender')
+plt.ylabel('Count')
+
+st.pyplot(plt)
+
+html_2 = """
+<div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
+<center><h3>The relationship between (Favorite Music Genre) and (Gender)</h3></center>
+</div>
+"""
+st.markdown(html_2, unsafe_allow_html=True)
+st.markdown("")
+
+
+plt.figure(figsize=(15,5))
+gender_data['Favorite Color'].value_counts().plot.bar(edgecolor='red', alpha=0.5)
+
+plt.title('Count of Favorite Colors')
+plt.xlabel('Favorite Color')
+plt.ylabel('Count')
+
+
+st.pyplot(plt)
---
+++ pages/1_Introduction.py
@@ -0,0 +1,31 @@
+import json
+import time
+import requests
+import streamlit as st
+
+st.set_page_config(
+    page_title="pattanan ",
+    page_icon= ":bar_chart:",
+)
+st.sidebar.success("เลือกรายการด้านบน.")
+
+
+html_1 = """
+<div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
+<center><h3> การพยากรณ์ข้อมูล </h3></center>
+</div>
+"""
+
+st.header("  การพยากรณ์ข้อมูล....!  ")
+st.subheader(" 1.หลักการและเหตุผล")
+st.info("""
+        กราฟนี้เเสดงเพือทราบเกี่ยวกับการที่ผู้หญิงเเละผู้ชายมีความชอบเเตกต่างกันเรื่องสีเช่น
+        Warm cool Neutral จึงนำมาทำการวิเคราะห์
+        <br>
+        
+        """)
+st.subheader(" 2.วัตถุประสงค์")
+st.info("""
+        เพื่อศึกษาเทคนิคการนำมาทำกราฟเกี่ยวกับความชอบเกี่ยวกับสีของผู้ชายเเละผู้หญิง
+        """)
+st.balloons()
