e24607fa0c88c87ff34414aca5ddab72f9efe529
jaonineX <neoking285@gmail.com>
2024-03-22 02:12:35 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,7 +4,13 @@ import seaborn as sns
 import pandas as pd
 
 gender_data=pd.read_csv('./data/superstore.csv')
-
+html_3 = """
+<div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
+<center><h3>การทำ Data Visualization เรื่องยอดขาย</h3></center>
+</div>
+"""
+st.markdown(html_3, unsafe_allow_html=True)
+st.markdown("")
 
 html_0 = """
 <div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
@@ -42,7 +48,7 @@ st.pyplot(plt)
 
 html_2 = """
 <div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
-<center><h3>Count of Category by Region</h3></center>
+<center><h3>ยอดขายของแต่ละภูมิภาค</h3></center>
 </div>
 """
 st.markdown(html_2, unsafe_allow_html=True)
@@ -52,7 +58,7 @@ st.markdown("")
 
 plt.figure(figsize=(15,5))
 sns.countplot(x='Region', data=gender_data )
-plt.title('Count of Category by Region')
+plt.title('ยอดขายของแต่ละภูมิภาค')
 plt.xlabel('Region')
 plt.ylabel('Sales')
 
