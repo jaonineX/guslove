47b99f52857fbd8ae4f11d2bb9340703a2ebf606
jaonineX <neoking285@gmail.com>
2024-03-22 01:54:12 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -22,7 +22,7 @@ st.write(gender_data.head(10))
 
 html_1 = """
 <div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
-<center><h3>The relationship between (Favorite Color) and (Gender)</h3></center>
+<center><h3>Count of Category by Region</h3></center>
 </div>
 """
 st.markdown(html_1, unsafe_allow_html=True)
@@ -31,9 +31,9 @@ st.markdown("")
 
 
 plt.figure(figsize=(15,5))
-sns.countplot(x='Gender', data=gender_data, hue='Favorite Color')
-plt.title('Count of Favorite Colors by Gender')
-plt.xlabel('Gender')
+sns.countplot(x='Region', data=gender_data, hue='Category')
+plt.title('Count of Category by Region')
+plt.xlabel('Region')
 plt.ylabel('Count')
 
 st.pyplot(plt)
